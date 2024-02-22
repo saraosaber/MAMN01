8d3216429a0bdf2b9f9c3c4e2bbe8942f34113bf
andreroxhage <an8603ro-s@student.lu.se>
2024-02-22 11:19:47 +0100

    InputHandler needs testing swipe up/down

--- app/src/main/java/com/example/myapplication/InputHandler.java
+++ app/src/main/java/com/example/myapplication/InputHandler.java
@@ -1,4 +1,63 @@
 package com.example.myapplication;
+import android.content.Context;
+import android.hardware.SensorManager;
 
-public class InputHandler {
+import android.view.View;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+
+// https://stackoverflow.com/questions/76882374/swipe-up-down-gesture-on-launcher-application
+public class InputHandler implements View.OnTouchListener {
+
+    private final GestureDetector gestureDetector;
+
+    public InputHandler(Context context, GestureDetector gestureDetector) {
+        gestureDetector = new GestureDetector(context, new GestureListener());
+
+        this.gestureDetector = gestureDetector;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(@NonNull MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+
+            if (Math.abs(diffY) > Math.abs(diffX)) {
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeDown();
+                    } else {
+                        onSwipeUp();
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public void onSwipeDown() {
+        System.out.println("Down!!!");
+    }
+
+    public void onSwipeUp() {
+        System.out.println("Up!!!");
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GestureDetector gestureDetector;
+    private InputHandler inputHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assuming your SwipeAreaView is defined in activity_main.xml
+        RelativeLayout swipeAreaView = findViewById(R.id.swipeAreaView);
+
+        gestureDetector = new GestureDetector(this, new GestureListener());
+        inputHandler = new InputHandler(this, gestureDetector);
+
+        // Set the InputHandler as the OnTouchListener for SwipeAreaView
+        swipeAreaView.setOnTouchListener(inputHandler);
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        // You can override gesture-related methods here if needed
     }
 }
