e616b6d526787f2ab385ed0e1aa264b09df521f8
lisblk <94108528+lisblk@users.noreply.github.com>
2024-05-07 20:19:00 +0200

    Update MainActivity.java
    bara lite kommentarer m lösningsförslag för restart button, ingen kodändring

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         // Initialize GameController with the Player and Obstacle instances
         SoundManager sm = new SoundManager(this);
         gameController = new GameController(this, sm, vibrator);
+        
+        //Om vi flyttar rad 29-35 till en egen metod, kallar den för "newGame" och 
+        //låter en knapp (kanske lika stor som skärmen, aka man bara klickar på skärmen) på startsidan anropa den metoden så tror jag det borde funka som smort??
+        //Då återstår dock problemet med att starta om för att man inte får tillbaka knappen + att den kanske ligger kvar på skärmen iofs... 
+        //Så tror vi behöver en 2a activity som är spelet, och att main activity är en knapp som leder till activity 2 där spelet startar av sig själv och 
+        //när man där återvänder man till main activity. 
         Thread gameControllerThread = new Thread(gameController);
         try { // dont remove, this allows all initialisation to complete before starting. otherwise things like sound manager wont work
             TimeUnit.MILLISECONDS.sleep(4000);
